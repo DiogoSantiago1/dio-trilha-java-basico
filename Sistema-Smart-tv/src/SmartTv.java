3a67756c37221e9d14cdd5f07a4c3e5e0b7f8d84
Diogo Sanntiago <diogo.santiago984@gmail.com>
2023-02-22 21:39:47 -0300

    atualizaçoes

--- Sistema-Smart-tv/src/SmartTv.java
+++ Sistema-Smart-tv/src/SmartTv.java
@@ -1,3 +1,36 @@
 public class SmartTv {
+    boolean ligada= false;
+    int canal= 1;
+    int volume=25;
+   
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+   
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("aumentando o volume para: " + volume);
+        //volume = volume + 1 ---- foi apenas uma abreviaçao.
+    }
     
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("diminuindo o volume para: " + volume);
+        //volume = volume - 1 ---- foi apenas uma abreviaçao.
+    }
+    
+    public void ligar(){
+        ligada=true;
+        
+    }
+    
+    public void desligar(){
+        ligada=false;
+    }
 }
