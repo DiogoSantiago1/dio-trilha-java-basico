ca25cc8d61197b681fa1c0273502cf4d35694859
Diogo Sanntiago <diogo.santiago984@gmail.com>
2023-03-07 22:26:55 -0300

    attlz

---
+++ exercicio loops e arrays/exercicios loops/src/Ex1_NomeIdade.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Ex1_NomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.println("nome:");
+            nome = scan.next();
+            if(nome.equals("0")) break;
+            System.out.println("idade:");
+            idade = scan.nextInt();
+        }
+        System.out.println("continue e responda certo fdpd");
+
+      
+    }
+    }
+
---
+++ exercicio loops e arrays/exercicios loops/src/Ex4_ParEImpar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0, quantImpares = 0;
+
+        System.out.println("quantiade de numeros: ");
+        quantNumeros = scan.nextInt();
+            int count = 0;
+        do {
+            System.out.println("numero:");
+            numero = scan.nextInt();
+            if (numero % 2 == 0) quantPares++;
+            else quantImpares++;
+
+            count++;
+        } while(count < quantNumeros);
+        System.out.println("Quantidade de numeros Pares:" + quantPares);
+        System.out.println("Quantiade de numeros Impares" + quantImpares);
+
+    }
+}
