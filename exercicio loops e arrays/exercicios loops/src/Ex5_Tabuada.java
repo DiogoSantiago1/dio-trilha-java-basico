2bf0c6e995c0823ddeba3189ad1932f01ac53c7a
Diogo Sanntiago <diogo.santiago984@gmail.com>
2023-03-07 23:13:57 -0300

    attlz

--- exercicio loops e arrays/exercicios loops/src/Ex5_Tabuada.java
+++ exercicio loops e arrays/exercicios loops/src/Ex5_Tabuada.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Ex5_Tabuada {
     public static void main(String[] args) {
-        
+       Scanner scan = new Scanner(System.in);
+       int numeroDaTabuada;
+       int numeroMultipilcado;
+       int count = 0;
+       System.out.println("Tabuada do:");
+       numeroDaTabuada = scan.nextInt();
+       System.out.println("Tabuada de " + numeroDaTabuada);
+        do {
+            count = count + 1;
+
+            System.out.println( numeroDaTabuada + " x " + count + " = " + numeroDaTabuada * count);
+        } while( count < 10 );
+
     }
 }
---
+++ exercicio loops e arrays/exercicios loops/src/Ex6_Fatorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(" Fatorial:");
+        int Fatorial = scan.nextInt();
+        int multiplicacao = 1;
+
+        for ( int i = Fatorial ; i >= 1; i-- ) {
+            multiplicacao = multiplicacao * i;
+    
+        }
+        System.out.println( Fatorial +"! =" + multiplicacao);
+    }
+}
