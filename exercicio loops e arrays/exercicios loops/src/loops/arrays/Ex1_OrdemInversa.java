9d6e5a039b44c378c9b052b40b3451296584b9e7
Diogo Sanntiago <diogo.santiago984@gmail.com>
2023-03-08 00:15:10 -0300

    atllz

--- exercicio loops e arrays/exercicios loops/src/loops/arrays/Ex1_OrdemInversa.java
+++ exercicio loops e arrays/exercicios loops/src/loops/arrays/Ex1_OrdemInversa.java
@@ -1,5 +1,22 @@
 package loops.arrays;
 
 public class Ex1_OrdemInversa {
-    
+    public static void main(String[] args) {
+        
+        int[] vetor = { -5, -6, 16, 4, 7, 9};
+        int count =0;
+        System.out.print("vetor: ");
+        while(count <= (vetor.length-1)) {
+
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+        System.out.print("\nvetor: ");
+        for( int i = (vetor.length - 1 ); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+
+        }
+
+
+    }
 }
